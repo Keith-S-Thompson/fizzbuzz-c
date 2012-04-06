295fed24017be742598fb339ff085955b1773578
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-06 11:50:12 -0700

    Switch statement

---
+++ fizzbuzz15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) {
+    for (int i = 1; i <= 100; i ++) {
+        switch (i%15) {
+            case 0:
+                puts("FizzBuzz");
+                break;
+            case 3: case 6: case 9: case 12:
+                puts("Fizz");
+                break;
+            case 5: case 10:
+                puts("Buzz");
+                break;
+            default:
+                printf("%d\n", i);
+                break;
+        }
+    }
+    return 0;
+}
