2374ed3be60475e19f4ce84a68a79ccfb0889612
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-07 14:46:54 -0700

    Loop unrolling

---
+++ fizzbuzz19.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+    for (int i = 1; ; i += 15) {
+        printf("%d\n%d\n", i, i+1);
+        puts("Fizz");
+        printf("%d\n", i+3);
+        puts("Buzz");
+        puts("Fizz");
+        printf("%d\n%d\n", i+6, i+7);
+        puts("Fizz\nBuzz");
+        if (i+10 > 100) break;
+        printf("%d\n", i+10);
+        puts("Fizz");
+        printf("%d\n%d\n", i+12, i+13);
+        puts("FizzBuzz");
+    }
+    return 0;
+}
