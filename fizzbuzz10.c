fa4c1e66c00a74cd3806b23cc2b5c6ac5cf09cd4
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-07 14:46:26 -0700

    Drop the dummy first element of the array

--- fizzbuzz10.c
+++ fizzbuzz10.c
@@ -6,16 +6,16 @@ void fizzbuzz (int n) { puts("FizzBuzz"); }
 void print    (int n) { printf("%d\n", n); }
 
 int main(void) {
-    void (*funcs[101])(int) = { 0 };
+    void (*funcs[100])(int) = { 0 };
 
-    for (int i = 3;  i <= 100; i += 3)  funcs[i] = fizz;
-    for (int i = 5;  i <= 100; i += 5)  funcs[i] = buzz;
-    for (int i = 15; i <= 100; i += 15) funcs[i] = fizzbuzz;
+    for (int i = 3;  i <= 100; i += 3)  funcs[i-1] = fizz;
+    for (int i = 5;  i <= 100; i += 5)  funcs[i-1] = buzz;
+    for (int i = 15; i <= 100; i += 15) funcs[i-1] = fizzbuzz;
     for (int i = 1; i <= 100; i ++) {
-        if (funcs[i] == NULL) funcs[i] = print;
+        if (funcs[i-1] == NULL) funcs[i-1] = print;
     }
 
-    for (int i = 1; i <= 100; i ++) funcs[i](i);
+    for (int i = 1; i <= 100; i ++) funcs[i-1](i);
 
     return 0;
 }
