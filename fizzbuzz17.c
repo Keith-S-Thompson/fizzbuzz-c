ca19a124532ce497e9502ac33080e4f3cd266105
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-07 11:07:50 -0700

    Fewer lines

--- fizzbuzz17.c
+++ fizzbuzz17.c
@@ -2,19 +2,13 @@
 
 void fizzbuzz(int lo, int hi) {
     if (lo <= hi) {
-        int i = (lo + hi) / 2;
-        if (lo < hi) {
-            fizzbuzz(lo, i-1);
-        }
-
-        if      (i % 15 == 0) { puts("FizzBuzz"); }
-        else if (i % 3  == 0) { puts("Fizz"); }
-        else if (i % 5  == 0) { puts("Buzz"); }
-        else                  { printf("%d\n", i); }
-
-        if (lo < hi) {
-            fizzbuzz(i+1, hi);
-        }
+        const int i = (lo + hi) / 2;
+        if (lo < hi) fizzbuzz(lo, i-1);
+        if      (i % 15 == 0) puts("FizzBuzz");
+        else if (i % 3  == 0) puts("Fizz");
+        else if (i % 5  == 0) puts("Buzz");
+        else                  printf("%d\n", i);
+        if (lo < hi) fizzbuzz(i+1, hi);
     }
 }
 
