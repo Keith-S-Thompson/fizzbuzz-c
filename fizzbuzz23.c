2568129131b693b9a8240532b9920c850ccf8977
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-09 13:38:42 -0700

    Use a two dimensional array of characters

---
+++ fizzbuzz23.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+    char line[100][9] = { 0 };
+    for (int i = 1; i <= 100; i ++ ) {
+        sprintf(line[i-1], "%d", i);
+    }
+    for (int i = 3; i <= 100; i += 3) {
+        strcpy(line[i-1], "Fizz");
+    }
+    for (int i = 5; i <= 100; i += 5) {
+        strcpy(line[i-1], "Buzz");
+    }
+    for (int i = 15; i <= 100; i += 15) {
+        strcpy(line[i-1], "FizzBuzz");
+    }
+    for (int i = 1; i <= 100; i ++) {
+        puts(line[i-1]);
+    }
+    return 0;
+}
