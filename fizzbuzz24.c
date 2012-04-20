619d0590a863e4672610feb5d08cca9cb8c3ed03
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-19 23:51:27 -0700

    String processing

---
+++ fizzbuzz24.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+    for (int i = 1; i <= 100; i ++) {
+        char line[9];
+        line[0] = '\0';
+        if (i % 3 == 0) {
+            strcat(line, "Fizz");
+        }
+        if (i % 5 == 0) {
+            strcat(line, "Buzz");
+        }
+        if (line[0] == '\0') {
+            sprintf(line, "%d", i);
+        }
+        puts(line);
+    }
+    return 0;
+}
