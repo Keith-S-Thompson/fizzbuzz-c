6c140bfe58a38bb3f0189e57cfe3c143bb781087
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-20 00:09:37 -0700

    Use printf's "%n" conversion specifier and a little tricky logic

---
+++ fizzbuzz25.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+    for (int i = 1; i <= 100; i ++) {
+        int n = 0;
+        i%3 || printf("%s%n", "Fizz", &n);
+        i%5 || printf("%s%n", "Buzz", &n);
+        n   || printf("%d", i);
+        putchar('\n');
+    }
+    return 0;
+}
