d0497bf362f867807d5f2e71c4b56f123ed483e9
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-06-26 14:47:13 -0700

    Manage digits separately

---
+++ fizzbuzz26.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void) {
+    for (int x = 0; x <= 10; x ++) {
+        for (int y = 0; y <= 9; y ++) {
+            if (x == 0 && y == 0) {
+                continue;
+            }
+            int printed = 0;
+            if ((x + y) % 3 == 0) {
+                fputs("Fizz", stdout);
+                printed = 1;
+            }
+            if (y == 0 || y == 5) {
+                fputs("Buzz", stdout);
+                printed = 1;
+            }
+            if (!printed) {
+                x && printf("%d", x);
+                y && putchar(y%10 + '0') || putchar('0');
+            }
+            putchar('\n');
+            if (x == 10 && y == 0) {
+                break;
+            }
+        }
+    }
+    return 0;
+}
