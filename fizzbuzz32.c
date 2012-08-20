c898b99d104a4466a8a33a7f4bcb32bf152938a2
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-08-19 21:37:29 -0700

    Tweak array indexing

--- fizzbuzz32.c
+++ fizzbuzz32.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 int main(void) {
-    char output[900] = { 0 };
-    for (int i = 2; i < 100; i += 3) {
-        strcpy(output + 9*i, "Fizz");
+    char output[900] = "";
+    for (int i = 18; i < 900; i += 27) {
+        strcpy(output + i, "Fizz");
     }
-    for (int i = 4; i < 100; i += 5) {
-        strcat(output + 9*i, "Buzz");
+    for (int i = 36; i < 900; i += 45) {
+        strcat(output + i, "Buzz");
     }
-    for (int i = 0; i < 100; i ++) {
-        if (!output[9*i]) {
-            sprintf(output + 9*i, "%d", i+1);
+    for (int i = 0; i < 900; i += 9) {
+        if (!output[i]) {
+            sprintf(output + i, "%d", i/9+1);
         }
     }
-    for (int i = 0; i < 100; i ++) {
-        puts(output + 9*i);
+    for (int i = 0; i < 900; i += 9) {
+        puts(output + i);
     }
     return 0;
 }
