7a2a983fde94f4dd3d0218e05a9c50e1964436d5
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-09-02 16:01:50 -0700

    Add fizzbuzz36.c, another brute-force approach

---
+++ fizzbuzz36.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+    for (int i = 1; i <= 100; i ++) {
+        switch ((i-1)["ddfdbfddfbdfddFddfdbfddfb"
+                      "dfddFddfdbfddfbdfddFddfdb"
+                      "fddfbdfddFddfdbfddfbdfddF"
+                      "ddfdbfddfbdfddFddfdbfddfb"])
+        {
+            case 'd': printf("%d\n", i); break;
+            case 'f': puts("Fizz");      break;
+            case 'b': puts("Buzz");      break;
+            case 'F': puts("FizzBuzz");  break;
+        }
+    }
+    return 0;
+}
