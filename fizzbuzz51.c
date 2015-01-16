ab40a0d31617216784983c39da3744a527ae7a7b
Keith Thompson <Keith.S.Thompson@gmail.com>
2015-01-16 08:04:33 -0800

    Add fizzbuzz51.c

---
+++ fizzbuzz51.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+    for (int i = 0; i < 100; i ++) {
+        switch ((i%15)["[[][\"][[]\"[][['"]) {
+            case '[':  printf("%d\n", i+1); break;
+            case ']':  puts("Fizz");        break;
+            case '"':  puts("Buzz");        break;
+            case '\'': puts("FizzBuzz");    break;
+        }
+    }
+}
