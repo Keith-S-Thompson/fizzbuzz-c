50d77e3d404f216d84a41508927117d2ff0b72f3
Keith Thompson <Keith.S.Thompson@gmail.com>
2015-12-05 20:30:55 -0800

    Add fizzbuzz68.c

--- README.md
+++ README.md
@@ -92,7 +92,7 @@ then unconditionally print a new-line character).
 
 And there are a number of other ways to solve the problem.
 
-This project contains, so far, 67 different C implementations of
+This project contains, so far, 68 different C implementations of
 FizzBuzz, most of them deliberately silly, using various combinations
 of the `?:` conditional operator, short-circuit `&&` and `||`, function
 pointers, arrays of function pointers, arrays of arrays of function
@@ -239,3 +239,5 @@ Please do not use these programs as examples of good programming style.
   A few macros can make the code much clearer
 * [fizzbuzz67.c](https://github.com/Keith-S-Thompson/fizzbuzz-c/blob/master/fizzbuzz67.c)
   Compound literal
+* [fizzbuzz68.c](https://github.com/Keith-S-Thompson/fizzbuzz-c/blob/master/fizzbuzz68.c)
+  Threads
---
+++ fizzbuzz68.c
@@ -0,0 +1,54 @@
+#define _POSIX_C_SOURCE 199309L
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int lines_printed = 0;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void try(int status) {
+    if (status != 0) {
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void *thread_routine(void *arg) {
+    const int i = *(int*)arg;
+    while (1) {
+        try(pthread_mutex_lock(&mutex));
+        if (lines_printed == i - 1) {
+            if      (i % 15 == 0) puts("FizzBuzz");
+            else if (i %  3 == 0) puts("Fizz");
+            else if (i %  5 == 0) puts("Buzz");
+            else                  printf("%d\n", i);
+            lines_printed ++;
+            free(arg);
+            try(pthread_mutex_unlock(&mutex));
+            pthread_exit(NULL);
+        }
+        else {
+            try(pthread_mutex_unlock(&mutex));
+        }
+        enum { MILLISECOND = 1000000 };
+    }
+}
+
+static void launch_thread(int i, pthread_t *thread) {
+    int *iptr = malloc(sizeof *iptr);
+    if (iptr == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    *iptr = i;
+    try(pthread_create(thread, NULL, thread_routine, iptr));
+}
+
+int main(void) {
+    pthread_t threads[100];
+    for (int i = 100; i >= 1; i --) {
+        launch_thread(i, &threads[i-1]);
+    }
+    for (int i = 1; i <= 100; i ++) {
+        try(pthread_join(threads[i-1], NULL));
+    }
+}
