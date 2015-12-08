6072828ee01825f08c3254af1b0b5059fddd3799
Keith Thompson <Keith.S.Thompson@gmail.com>
2015-12-08 12:44:16 -0800

    Who says declarations can't have labels?

--- fizzbuzz65.c
+++ fizzbuzz65.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(void) {
-int i = 1;
-_10:  if (i > 100) goto _140;
-_20:  if (i % 15 == 0) goto _110;
-_30:  if (i % 3 == 0) goto _70;
-_40:  if (i % 5 == 0) goto _90;
-_50:  printf("%d\n", i);
-_60:  goto _120;
-_70:  puts("Fizz");
-_80:  goto _120;
-_90:  puts("Buzz");
-_100: goto _120;
-_110: puts("FizzBuzz");
-_120: i = i + 1;
-_130: goto _10;
-_140: ; }
+_10:; int i = 1;
+_20:  if (i > 100) goto _150;
+_30:  if (i % 15 == 0) goto _120;
+_40:  if (i % 3 == 0) goto _80;
+_50:  if (i % 5 == 0) goto _100;
+_60:  printf("%d\n", i);
+_70:  goto _130;
+_80:  puts("Fizz");
+_90:  goto _130;
+_100:  puts("Buzz");
+_110: goto _130;
+_120: puts("FizzBuzz");
+_130: i = i + 1;
+_140: goto _20;
+_150:;}
