c8b6ac79c7c45dfeac627b3464758e2f72481b34
Keith Thompson <Keith.S.Thompson@gmail.com>
2017-03-20 16:36:04 -0700

    fizzbuzz87.c

---
+++ fizzbuzz87.c
@@ -0,0 +1,90 @@
+#define uf {
+#define Fb ;
+#define ZI int
+#define Ff 15
+#define Zf "Fizz"
+#define ZB ++
+#define UI puts
+#define FF 0
+#define FZ (
+#define Bz 5
+#define FI 0
+#define uz )
+#define Zz if
+#define UZ else
+#define fF if
+#define zI i
+#define ZF 100
+#define uF else
+#define IF i
+#define zB )
+#define Uf i
+#define Iz )
+#define IB <=
+#define bu %
+#define ub printf
+#define UF ;
+#define Zu }
+#define uu {
+#define BI puts
+#define ZU i
+#define BZ ;
+#define Fu )
+#define fz {
+#define Uz for
+#define fZ )
+#define zf (
+#define FU }
+#define FB 0
+#define If 3
+#define bf void
+#define Fz ;
+#define bZ <stdio.h>
+#define UU ;
+#define uI {
+#define bB "%d\n"
+#define BF {
+#define ff %
+#define zz ,
+#define zF main
+#define bI (
+#define fu (
+#define uB "Buzz"
+#define bz }
+#define UB i
+#define Bb }
+#define fB puts
+#define Uu ==
+#define Zb )
+#define bF }
+#define IU ==
+#define Ub i
+#define zU (
+#define zu ==
+#define uZ "FizzBuzz"
+#define bU )
+#define Bf (
+#define bb )
+#define zb (
+#define BB }
+#define fb =
+#define ZZ if
+#define Ib {
+#define uU else
+#define Iu i
+#define II (
+#define fU int
+#define zZ (
+#define BU )
+#define IZ %
+#define fI ;
+#define Bu 1
+#include bZ
+fU zF FZ bf zB uI Uz bI ZI UB
+fb Bu fI Iu IB ZF Fz zI ZB BU
+uu ZZ II Uf IZ Ff Uu FB fZ Ib
+fB fu uZ uz UF bF uU fF Bf ZU
+ff If zu FI Iz BF BI zf Zf Fu
+BZ Zu UZ Zz zb IF bu Bz IU FF
+Zb fz UI zZ uB bb Fb Bb uF uf
+ub zU bB zz Ub bU UU BB bz FU
