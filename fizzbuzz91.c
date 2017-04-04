7390acb2a88d3e42581a5be9b2381a0f81fbf68d
Keith Thompson <Keith.S.Thompson@gmail.com>
2017-04-04 08:55:23 -0700

    fizzbuzz91.c

---
+++ fizzbuzz91.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+    const unsigned char lines[100] =
+        "\1\2F\4BF\7\10FB\13F\15\16Z"
+        "\20\21F\23BF\26\27FB\32F\34\35Z"
+        "\37 F\"BF%&FB)F+,Z"
+        "./F1BF45FB8F:;Z"
+        "=>F@BFCDFBGFIJZ"
+        "LMFOBFRSFBVFXYZ"
+        "[\\F^BFabFB";
+    for (int i = 0; i < sizeof lines; i ++) {
+        lines[i] == 'F' && puts("Fizz") ||
+        lines[i] == 'B' && puts("Buzz") ||
+        lines[i] == 'Z' && puts("FizzBuzz") ||
+        printf("%d\n", lines[i]);
+    }
+}
