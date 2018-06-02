484c714f41c9a24cf8071a79876f5e1ac8fe4a2f
Keith Thompson <Keith.S.Thompson@gmail.com>
2018-06-02 15:36:38 -0700

    Tweak fizzbuzz102.c

--- fizzbuzz102.c
+++ fizzbuzz102.c
@@ -3,7 +3,7 @@
 int main(void) {
     for (int i = 1; i <= 100; i ++) {
         char line[10];
-        sprintf(line, "%s%s%d", i % 3 ? "" : "Fizz", i % 5 ? "" : "Buzz", (i % 3 && i % 5) ? i*10 : 0);
+        sprintf(line, "%s%s%d", i % 3 ? "" : "Fizz", i % 5 ? "" : "Buzz", i * 10 * (i % 3 && i % 5));
         *strchr(line, '0') = '\0';
         puts(line);
     }
