f7e593dc97d146cd80f2ff22bca6d069ae604cfb
Keith Thompson <Keith.S.Thompson@gmail.com>
2018-09-15 12:36:02 -0700

    fizzbuzz031.c: Fix evaluation order dependency

--- fizzbuzz031.c
+++ fizzbuzz031.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void) {
     for (int i = 1; i <= 100; i ++) {
-        printf(i % 3 ? "" : "Fizz") + printf(i % 5 ? "" : "Buzz") || printf("%d", i);
+        const int three = printf(i % 3 ? "" : "Fizz");
+        three + printf(i % 5 ? "" : "Buzz") || printf("%d", i);
         putchar('\n');
     }
 }
