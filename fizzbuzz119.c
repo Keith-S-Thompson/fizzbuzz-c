ac82bbc1bb7a2750e0e85f53cbaafb8bba6ca855
Keith Thompson <Keith.S.Thompson@gmail.com>
2019-01-21 22:14:05 -0800

    fizzbuzz119.c: Remove explanatory comment

--- fizzbuzz119.c
+++ fizzbuzz119.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-/*
- * In principle, we could compute a value that indicates what to
- * print for each line by fitting a 99th-degree real polynomial to
- * the 100 data points.  Since the output repeats in a cyle of 15
- * (using the same value for lines on which the number is printed),
- * it's easier to fit a 14th-degree polynomial to 15 data points.
- *
- * http://www.xuru.org/rt/PR.asp can fit a polynomial of up to 10th
- * degree for a given set of data points, which is not quite enough.
- * The result, when rounded to the nearest integer, was correct
- * except for an input of 0, which yielded approximately -178 rather
- * than 3.
- *
- * The use of pow() to compute integer powers is admittedly
- * inefficient.
- */
-
 static double polynomial(long long n) {
     return - 4.956564401E-6 * pow(n, 10)
            + 3.717551624E-4 * pow(n, 9)
