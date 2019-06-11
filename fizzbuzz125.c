6976aa70cf029635f246398d997e7a85334d2d99
Keith Thompson <Keith.S.Thompson@gmail.com>
2019-06-10 19:49:56 -0700

    fizzbuzz125.c: Set and use __FILE__

---
+++ fizzbuzz125.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+int main(void) {
+#line 1 "1"
+    puts(__FILE__);
+#line 1 "2" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "4" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "7" 
+    puts(__FILE__);
+#line 1 "8" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "11" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "13" 
+    puts(__FILE__);
+#line 1 "14" 
+    puts(__FILE__);
+#line 1 "FizzBuzz" 
+    puts(__FILE__);
+#line 1 "16" 
+    puts(__FILE__);
+#line 1 "17" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "19" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "22" 
+    puts(__FILE__);
+#line 1 "23" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "26" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "28" 
+    puts(__FILE__);
+#line 1 "29" 
+    puts(__FILE__);
+#line 1 "FizzBuzz" 
+    puts(__FILE__);
+#line 1 "31" 
+    puts(__FILE__);
+#line 1 "32" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "34" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "37" 
+    puts(__FILE__);
+#line 1 "38" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "41" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "43" 
+    puts(__FILE__);
+#line 1 "44" 
+    puts(__FILE__);
+#line 1 "FizzBuzz" 
+    puts(__FILE__);
+#line 1 "46" 
+    puts(__FILE__);
+#line 1 "47" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "49" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "52" 
+    puts(__FILE__);
+#line 1 "53" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "56" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "58" 
+    puts(__FILE__);
+#line 1 "59" 
+    puts(__FILE__);
+#line 1 "FizzBuzz" 
+    puts(__FILE__);
+#line 1 "61" 
+    puts(__FILE__);
+#line 1 "62" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "64" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "67" 
+    puts(__FILE__);
+#line 1 "68" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "71" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "73" 
+    puts(__FILE__);
+#line 1 "74" 
+    puts(__FILE__);
+#line 1 "FizzBuzz" 
+    puts(__FILE__);
+#line 1 "76" 
+    puts(__FILE__);
+#line 1 "77" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "79" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "82" 
+    puts(__FILE__);
+#line 1 "83" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "86" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "88" 
+    puts(__FILE__);
+#line 1 "89" 
+    puts(__FILE__);
+#line 1 "FizzBuzz" 
+    puts(__FILE__);
+#line 1 "91" 
+    puts(__FILE__);
+#line 1 "92" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "94" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "97" 
+    puts(__FILE__);
+#line 1 "98" 
+    puts(__FILE__);
+#line 1 "Fizz" 
+    puts(__FILE__);
+#line 1 "Buzz" 
+    puts(__FILE__);
+} 
