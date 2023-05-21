e22ba9010982b67d3e7fcbc512074935c64de141
Keith Thompson <Keith.S.Thompson@gmail.com>
2023-05-20 19:19:43 -0700

    fizzbuzz131.c - Add a hack to allow for differing fputs() behavior.
    On Ubuntu (glibc), a successful fputs() call returns 1.
    On Cygwin (newlib) it returns 0.
    Both are conforming.
    
    Add 1 to the result to ensure that it's truthy.

--- fizzbuzz131.c
+++ fizzbuzz131.c
@@ -3,8 +3,8 @@
 int main(void) {
     for (int i = 1; i <= 100; i ++) {
         bool by3, by5;
-        by3 = i % 3 == 0 && fputs("Fizz", stdout);
-        by5 = i % 5 == 0 && fputs("Buzz", stdout);
+        by3 = i % 3 == 0 && 1+fputs("Fizz", stdout);
+        by5 = i % 5 == 0 && 1+fputs("Buzz", stdout);
         by3 || by5 || printf("%d", i);
         putchar('\n');
     }
