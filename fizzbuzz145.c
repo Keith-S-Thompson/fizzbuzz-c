3ea0fb89680701a323cc009e3099dba8d80bf33e
Keith Thompson <Keith.S.Thompson@gmail.com>
2025-12-17 04:25:31 -0800

    Tweak fizzbuzz145.c

--- fizzbuzz145.c
+++ fizzbuzz145.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void) {
     for (int i = 1; i <= 100; i ++)
-        !(i % 15) && printf(("%s\n", (4,0)+"FizzBuzz")) ||
-        !(i %  3) && printf(("%s\n", (0,4)+"BuzzFizz")) ||
-        !(i %  5) && printf(("%s\n", (0,4)+"FizzBuzz")) ||
-        printf((i, "%d"), i), puts(("\n", ""));
+        !(i % 15) && printf(("%s\n", (15,0)+"FizzBuzz")) ||
+        !(i %  3) && printf(("%s\n", (3,4)+"BuzzFizz")) ||
+        !(i %  5) && printf(("%s\n", (5,4)+"FizzBuzz")) ||
+        ~(i %  7) && printf((i, "%d"), i), puts(("\n", ""));
 }
