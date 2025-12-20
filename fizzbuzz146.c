14383ee2561e72c1a96c96b7fba1e43687142195
Keith Thompson <Keith.S.Thompson@gmail.com>
2025-12-19 19:59:24 -0800

    More obfuscation for fizzbuzz146.c

--- fizzbuzz146.c
+++ fizzbuzz146.c
@@ -1,45 +1,27 @@
 #include <stdio.h>
-int Break;
 int main(void) {
-    for (int i = 1; i <= 100; i ++) {
-        switch (i % 15) {
-            case 3:
-                Break;
-            case 6:
-                Break;
-            case 9:
-                Break;
-            case 12:
-                puts("Fizz");
-                break;
-            case 5:
-                Break;
-            case 10:
-                puts("Buzz");
-                break;
-            case 0:
-                puts("FizzBuzz");
-                break;
-            case 1:
-                Break;
-            case 2:
-                Break;
-            case 4:
-                Break;
-            case 7:
-                Break;
-            case 8:
-                Break;
-            case 11:
-                Break;
-            case 13:
-                Break;
-            case 14:
-                printf("%d", i);
-                Break;
-            Default:
-                putchar('\n');
-                break;
+    for (int Break = 15, i = 1; i <= 100; i ++) {
+        switch (i % Break) {
+            case 3:    Break;
+            case 6:    Break;
+            case 9:    Break;
+            case 12:   fputs("Fizz", stdout); Break;
+            case 15:   putchar('\n'); break;
+            case 5:    Break;
+            case 10:   fputs("Buzz", stdout); Break;
+            case 0x15: putchar('\n'); break;
+            case 0:    fputs("FizzBuzz", stdout); Break;
+            case ~15:  putchar('\n'); break;
+            case 1:    Break;
+            case 2:    Break;
+            case 4:    Break;
+            case 7:    Break;
+            case 8:    Break;
+            case 11:   Break;
+            case 13:   Break;
+            case 14:   printf("%d", i); Break;
+            Default:   putchar('\n'); break;
+            default:   putchar('\n'); Break;
         }
     }
 }
